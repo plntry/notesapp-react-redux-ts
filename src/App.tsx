b0affc13fccc4f08ff5f2678a2d2da22ceab4d27
Oksana Nedilko <nedilko7b@gmail.com>
2022-09-15 10:49:40 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <div>
+        <input type='text' name='note' placeholder='Note'/>
+        <button>Add note</button>
+      </div>
+      <hr/>
+      <ul>
+        <li>Some note</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
