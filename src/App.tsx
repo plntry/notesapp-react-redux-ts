77a9e603237d9e862a9b4b6a6546c2e2c0eb8ff4
Oksana Nedilko <nedilko7b@gmail.com>
2022-09-28 16:51:03 +0300

    Changed browser router to the hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route} from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchNotes } from './redux/actions/noteListActions';
 import { CreateNewNote } from './components/notes-components/CreateNewNote';
